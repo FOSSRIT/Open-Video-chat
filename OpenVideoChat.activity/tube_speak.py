d52ac738d655417772c89e8bd88fe3853dcc651c
jlew <jlew.blackout@gmail.com>
2010-03-29 20:54:36 -0400

    Opps, need to import socket

--- OpenVideoChat.activity/tube_speak.py
+++ OpenVideoChat.activity/tube_speak.py
@@ -7,6 +7,7 @@ PATH = "/org/laptop/OpenVideoChat"
 
 # http://code.activestate.com/recipes/439094-get-the-ip-address-associated-with-a-network-inter/
 def get_ip_address(ifname):
+    import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
