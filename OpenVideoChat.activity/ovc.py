dd83540ebe00b23c9e3057528c367a66f666fcf3
jlew <jlew.blackout@gmail.com>
2010-04-19 20:36:21 -0400

    Interface no longer hard coded.
    Finds first interface that is not lo that will return a valid ip
    Hopefully fixes bug: https://fedorahosted.org/OpenVideoChat/ticket/44

--- OpenVideoChat.activity/ovc.py
+++ OpenVideoChat.activity/ovc.py
@@ -145,11 +145,12 @@ class OpenVideoChatActivity(Activity):
         elif src == "join":
             handle = self.netstack.get_tube_handle()
             if handle:
+                import socket
+                import fcntl
+                import struct
+                import array
                 # http://code.activestate.com/recipes/439094-get-the-ip-address-associated-with-a-network-inter/
                 def get_ip_address(ifname):
-                    import socket
-                    import fcntl
-                    import struct
                     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                     return socket.inet_ntoa(fcntl.ioctl(
                         s.fileno(),
@@ -157,8 +158,32 @@ class OpenVideoChatActivity(Activity):
                         struct.pack('256s', ifname[:15])
                     )[20:24])
 
-                handle.announce_ip(get_ip_address('eth0'))
+                #http://code.activestate.com/recipes/439093-get-names-of-all-up-network-interfaces-linux-only/
+                def all_interfaces():
+                    max_possible = 128  # arbitrary. raise if needed.
+                    bytes = max_possible * 32
+                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    names = array.array('B', '\0' * bytes)
+                    outbytes = struct.unpack('iL', fcntl.ioctl(
+                        s.fileno(),
+                        0x8912,  # SIOCGIFCONF
+                        struct.pack('iL', bytes, names.buffer_info()[0])
+                    ))[0]
+                    namestr = names.tostring()
+                    return [namestr[i:i+32].split('\0', 1)[0] for i in range(0, outbytes, 32)]
 
+
+                for interface in all_interfaces():
+                    if interface != 'lo':
+                        try:
+                            ip = get_ip_address(interface)
+                            handle.announce_ip(ip)
+                            break
+                        except:
+                            print "Interface %s did not give ip" % interface
+                else:
+                    print "Could not find ip address"
+                    
         elif src == "ip":
             self.setup_outgoing_pipeline( args )
 
