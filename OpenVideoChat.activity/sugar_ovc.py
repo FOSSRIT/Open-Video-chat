3712ce24e99ae4e11d90758c161a06cf4f0f50e7
Casey DeLorme <CDeLorme@gmail.com>
2013-06-27 10:34:58 -0400

    Updated some minor path information in comments.

--- OpenVideoChat.activity/sugar_ovc.py
+++ OpenVideoChat.activity/sugar_ovc.py
@@ -13,7 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with OpenVideoChat.  If not, see <http://www.gnu.org/licenses/>.
 """
-:mod: `OpenVideoChat/OpenVideoChat.activity/sugar_ovc` -- Open Video Chat
+:mod: `OpenVideoChat.activity/sugar_ovc` -- Open Video Chat
 =======================================================================
 
 .. moduleauthor:: Justin Lewis <jlew.blackout@gmail.com>
