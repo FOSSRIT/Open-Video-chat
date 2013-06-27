00c78260b2e8824d002598b87516513249f8cda4
Casey DeLorme <CDeLorme@gmail.com>
2013-06-27 11:24:52 -0400

    Revised instantiation process and eliminated toolbar and related constants.
    Added documentation references to comments.
    
    Added logging messages.

--- OpenVideoChat.activity/gui.py
+++ OpenVideoChat.activity/gui.py
@@ -33,12 +33,6 @@ logger.setLevel(logging.DEBUG)
 
 
 # Constants
-ICONS = {
-    'play': 'media-playback-start-insensitive.svg',
-    'stop': 'media-playback-stop-insensitive.svg',
-    'unmute': 'speaker-100.svg',
-    'mute': 'speaker-000.svg'
-}
 MIN_CHAT_HEIGHT = 160
 MAX_CHAT_MESSAGE_SIZE = 200
 
@@ -46,64 +40,57 @@ MAX_CHAT_MESSAGE_SIZE = 200
 class Gui(Gtk.Grid):
     def __init__(self):
 
-        # Add Toolbar
-        self.build_toolbar()
-
         # Add Video
-        self.build_video()
+        logger.debug("Adding Video")
+        self.attach(self.build_video(), 0, 1, 1, 1)
 
         # Add Chat
-        self.build_chat()
+        logger.debug("Adding Chat")
+        self.attach(self.build_chat(), 0, 2, 1, 1)
 
         # Display Grid
         self.show()
 
-    def build_toolbar(self):
-
-        # Create Toolbar & Attach to Expander
-        self.toolbar = Gtk.Toolbar()
-
-        # Create Toggles
-        self.toggles = {
-            'outgoing-video': Gtk.ToolButton(sensitive=False, tooltip_text="Outgoing Video", icon_widget=Gtk.Image(file=ICONS['stop'])),
-            'outgoing-audio': Gtk.ToolButton(sensitive=False, tooltip_text="Outgoing Audio", icon_widget=Gtk.Image(file=ICONS['mute'])),
-            'incoming-video': Gtk.ToolButton(sensitive=False, tooltip_text="Incoming Video", icon_widget=Gtk.Image(file=ICONS['stop'])),
-            'incoming-audio': Gtk.ToolButton(sensitive=False, tooltip_text="Incoming Audio", icon_widget=Gtk.Image(file=ICONS['mute']))
-        }
-
-        # Append to Window
-        self.toolbar.insert(self.toggles['outgoing-video'], 0)
-        self.toolbar.insert(self.toggles['outgoing-audio'], 1)
-        self.toolbar.insert(self.toggles['incoming-video'], 2)
-        self.toolbar.insert(self.toggles['incoming-audio'], 3)
-
-        # Create Expander, add toolbar, connect to grid, and display
-        self.toolbar_expander = Gtk.Expander(expanded=True)
-        self.toolbar.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(.01, .01, .01, .9))
-        self.toolbar_expander.add(self.toolbar)
-        self.attach(self.toolbar_expander, 0, 0, 1, 1)
-        self.toolbar_expander.show_all()
-
     def build_video(self):
 
-        # Create a Gtk Drawing Area & Append to grid then display
-        self.video = Gtk.DrawingArea(vexpand=True, hexpand=True)
-        self.video.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(.01, .01, .01, .8))
-        self.attach(self.video, 0, 1, 1, 1)
-        self.video.show()
+        # Create [GtkDrawingArea](https://developer.gnome.org/gtk3/3.0/GtkDrawingArea.html) & [Modify Background](https://developer.gnome.org/gtk3/3.0/gtk-question-index.html)
+        self.video = video = Gtk.DrawingArea(vexpand=True, hexpand=True)
+        video.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(.01, .01, .01, .9))
+        video.show()
+
+        # Return Video Component
+        return video
 
     def build_chat(self):
 
         # Create Chat Components
-        self.chat_text_buffer = Gtk.TextBuffer()
-        self.chat_text_view = Gtk.TextView(editable=False, buffer=self.chat_text_buffer, cursor_visible=False, wrap_mode=Gtk.WrapMode.WORD)
-        self.chat_scrollable_history = Gtk.ScrolledWindow(hexpand=True, hscrollbar_policy=Gtk.PolicyType.NEVER, vscrollbar_policy=Gtk.PolicyType.AUTOMATIC, min_content_height=MIN_CHAT_HEIGHT)
-        self.chat_scrollable_history.add(self.chat_text_view)
-        self.chat_entry = Gtk.Entry(hexpand=True, max_length=MAX_CHAT_MESSAGE_SIZE)
-        self.chat_entry.connect("activate", self.send_message)
-        self.chat_send_message_button = Gtk.Button(_("Send"))
-        self.chat_send_message_button.connect("clicked", self.send_message)
+        self.chat_text_buffer = chat_text_buffer = Gtk.TextBuffer()
+        chat_text_view = Gtk.TextView(editable=False, buffer=chat_text_buffer, cursor_visible=False, wrap_mode=Gtk.WrapMode.WORD)
+        chat_scrollable_history = Gtk.ScrolledWindow(hexpand=True, hscrollbar_policy=Gtk.PolicyType.NEVER, vscrollbar_policy=Gtk.PolicyType.AUTOMATIC, min_content_height=MIN_CHAT_HEIGHT)
+        chat_scrollable_history.add(chat_text_view)
+        chat_entry = Gtk.Entry(hexpand=True, max_length=MAX_CHAT_MESSAGE_SIZE)
+        chat_entry.connect("activate", self.send_message)
+        chat_send_message_button = Gtk.Button(_("Send"))
+        chat_send_message_button.connect("clicked", self.send_message)
 
+        # Create Grid and Append Chat Components
+        chat_grid = Gtk.Grid()
+        chat_grid.attach(chat_scrollable_history, 0, 0, 2, 1)
+        chat_grid.attach(chat_entry, 0, 1, 1, 1)
+        chat_grid.attach(chat_send_message_button, 1, 1, 1, 1)
+
+        # Add User List (Multi-User Feature - Not yet ready for implementation)
+        # self.chat_grid.attach(self.build_user_list(), 2, 0, 1, 1)
+
+        # Create Expander, Add Grid & Display
+        chat_expander = Gtk.Expander(expanded=True, label=_("Chat"))
+        chat_expander.add(chat_grid)
+        chat_expander.show_all()
+
+        # Return Attachable Component
+        return chat_expander
+
+    def build_user_list(self):
         # # Create User List Components
         # self.user_list_search_entry = Gtk.Entry(max_length=MAX_CHAT_MESSAGE_SIZE)
         # self.user_list_search_button = Gtk.Button(_("Search"))
@@ -115,19 +102,8 @@ class Gui(Gtk.Grid):
         # self.user_list_expander = Gtk.Expander(label=_("Users"))
         # self.user_list_expander.add(self.user_list_grid)
         # self.user_list_expander.show_all()
-
-        # Create Grid and Append Chat Components
-        self.chat_grid = Gtk.Grid()
-        self.chat_grid.attach(self.chat_scrollable_history, 0, 0, 2, 1)
-        self.chat_grid.attach(self.chat_entry, 0, 1, 1, 1)
-        self.chat_grid.attach(self.chat_send_message_button, 1, 1, 1, 1)
-        # self.chat_grid.attach(self.user_list_expander, 2, 0, 1, 1)
-
-        # Create Expander & Add Grid to Expander and Expander to Layout Grid
-        self.chat_expander = Gtk.Expander(expanded=True, label=_("Chat"))
-        self.chat_expander.add(self.chat_grid)
-        self.attach(self.chat_expander, 0, 2, 1, 1)
-        self.chat_expander.show_all()
+        return False
 
     def send_message(self, sender):
+        # Send a message over the tubes
         return False
