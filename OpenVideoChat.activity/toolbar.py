1813bd54af150d5abad1202bd6c0106af754662b
Casey DeLorme <CDeLorme@gmail.com>
2013-06-27 10:53:51 -0400

    Defined basic toolbar component.

---
+++ OpenVideoChat.activity/toolbar.py
@@ -0,0 +1,35 @@
+#    This file is part of OpenVideoChat.
+#
+#    OpenVideoChat is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    OpenVideoChat is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with OpenVideoChat.  If not, see <http://www.gnu.org/licenses/>.
+"""
+:mod: `OpenVideoChat.activity/toolbar` -- Open Video Chat
+=======================================================================
+
+.. moduleauthor:: Casey DeLorme <cxd4280@rit.edu>
+"""
+
+# External Imports
+import logging
+from gi.repository import Gtk
+
+
+# Define Logger for Logging & DEBUG level for Development
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class Toolbar():
+    def __init__(self):
+        # Do Something
+        return True
