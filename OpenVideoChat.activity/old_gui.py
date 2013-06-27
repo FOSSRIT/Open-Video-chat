33cea67b318289d9aa5d871cb4cc0d729398fffe
Casey DeLorme <CDeLorme@gmail.com>
2013-06-27 12:08:45 -0400

    Slowly tearing apart old gui components and removing them from old gui

--- OpenVideoChat.activity/old_gui.py
+++ OpenVideoChat.activity/old_gui.py
@@ -13,7 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with OpenVideoChat.  If not, see <http://www.gnu.org/licenses/>.
 """
-:mod: `OpenVideoChat/OpenVideoChat.activity/gui` -- Open Video Chat Gui
+:mod: `OpenVideoChat.activity/gui` -- Open Video Chat Gui
 =======================================================================
 
 .. moduleauthor:: Justin Lewis <jlew.blackout@gmail.com>
@@ -54,19 +54,6 @@ class Gui(Gtk.Grid):
     def __init__(self, activity):
         Gtk.Grid.__init__(self)
 
-        # Set Activity Title
-        activity.set_title(_("OpenVideoChat"))
-
-        # Add Video & Chat Containers
-        self.add(self.build_video())
-        self.attach(self.build_chat(), 0, 1, 1, 1)
-
-        # Create & Apply Toolbar to Activity
-        activity.set_toolbar_box(self.build_toolbar(activity))
-
-        # Add Resize Event
-        self.connect('check-resize', self.resized)
-
         # Disable GUI Components until network connections established
         self.disable_net_options()
         self.disable_gst_options()
@@ -76,7 +63,6 @@ class Gui(Gtk.Grid):
 
         # Status Message
         logger.debug("Finished Preparing GUI")
-
 
     """ Resource Methods """
 
@@ -114,130 +100,9 @@ class Gui(Gtk.Grid):
         # else:
         #     self.movie_window_incoming.connect('realize', self.render_incoming)
 
-    """ GUI Component Establishment """
-
-    def build_video(self):
-        # Prepare Video Display
-        self.movie_window_preview = Gtk.DrawingArea()
-        # self.movie_window_incoming = Gtk.DrawingArea()
-
-        # Use Fixed to append overlays to an overlay
-        # video_fixed = Gtk.Fixed()
-        # video_fixed.put(self.movie_window_incoming, 0, 0)
-        # video_fixed.put(self.movie_window_preview, 0, 0)
-        # video_fixed.set_halign(Gtk.Align.START)
-        # video_fixed.set_valign(Gtk.Align.START)
-        #self.movie_window_preview.show()
-        #video_fixed.show()
-
-
-        # Use event box for background coloring
-        video_eventbox = Gtk.EventBox()
-        video_eventbox.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(.01,.01,.01,.8))
-        video_eventbox.set_hexpand(True)
-        video_eventbox.set_vexpand(True)
-        # video_eventbox.add(video_fixed)
-        video_eventbox.add(self.movie_window_preview)
-        video_eventbox.show_all()
-
-        # Return Overlay Container
-        return video_eventbox
-
-    def build_chat(self):
-        # Prepare Chat Text Container
-        self.chat_text = Gtk.TextBuffer()
-        self.text_view = Gtk.TextView()
-        self.text_view.set_buffer(self.chat_text)
-        self.text_view.set_editable(False)
-        self.text_view.set_cursor_visible(False)
-        self.text_view.set_wrap_mode(Gtk.WrapMode.WORD)
-
-        # Prepare Scrollable History
-        chat_history = Gtk.ScrolledWindow()
-        chat_history.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        chat_history.set_min_content_height(MIN_CHAT_HEIGHT)
-        chat_history.add(self.text_view)
-        chat_history.set_hexpand(True)
-
-        # Send button to complete feel of a chat program
-        self.chat_entry = Gtk.Entry()
-        self.chat_entry.set_max_length(MAX_MESSAGE_SIZE)
-        self.chat_entry.connect("activate", self.send_message)
-        self.chat_entry.set_hexpand(True)
-        self.chat_send_button = Gtk.Button(_("Send"))
-        self.chat_send_button.connect("clicked", self.send_message)
-
-        # Wrap expanded Entry and normal-sized Send buttons into Grid Row
-        chat_grid = Gtk.Grid()
-        chat_grid.attach(chat_history, 0, 0, 2, 1)
-        chat_grid.attach(self.chat_entry, 0, 1, 1, 1)
-        chat_grid.attach(self.chat_send_button, 1, 1, 1, 1)
-
-        # Chat expander allows visibly toggle-able container for all chat components
-        chat_expander = Gtk.Expander()
-        chat_expander.set_label(_("Chat"))
-        chat_expander.set_expanded(True)
-        chat_expander.add(chat_grid)
-
-        # Display all chat components
-        chat_expander.show_all()
-
-        # Return entire expander
-        return chat_expander
-
-    def build_toolbar(self, activity):
-        # Prepare Primary Toolbar Container
-        toolbar_box = ToolbarBox();
-
-        # Create activity button
-        toolbar_box.toolbar.insert(ActivityButton(activity), -1)
-
-        # Video Toggle
-        video_toggle_button = ToolButton()
-        video_toggle_button.connect("clicked", self.toggle_video)
-        toolbar_box.toolbar.insert(video_toggle_button, 1)
-        self.toggle_video(video_toggle_button)
-
-        # Audio Toggle
-        audio_toggle_button = ToolButton()
-        audio_toggle_button.connect("clicked", self.toggle_audio)
-        toolbar_box.toolbar.insert(audio_toggle_button, 2)
-        self.toggle_audio(audio_toggle_button)
-
-        # Toggle Preview Display Button
-        preview_toggle_button = ToolButton()
-        preview_toggle_button.connect("clicked", self.toolbar_toggle_preview_visibility)
-        toolbar_box.toolbar.insert(preview_toggle_button, 3)
-        self.toolbar_toggle_preview_visibility(preview_toggle_button)
-
-        # Forced Refresh
-        reload_video = ToolButton("view-refresh")
-        reload_video.set_tooltip_text(_("Reload Video"))
-        reload_video.connect("clicked", self.force_redraw)
-        toolbar_box.toolbar.insert(reload_video, -1)
-
-        # Push stop button to far right
-        separator = Gtk.SeparatorToolItem()
-        separator.props.draw = False
-        separator.set_expand(True)
-        toolbar_box.toolbar.insert(separator, -1)
-
-        # Create Share Button
-        toolbar_box.toolbar.insert(ShareButton(activity), -1)
-
-        # Create stop button
-        toolbar_box.toolbar.insert(StopButton(activity), -1)
-
-        # Add reference to toolbar items
-        self.toolbar = toolbar_box.toolbar
-
-        # Display all components & Return
-        toolbar_box.show_all()
-        return toolbar_box
-
     def toggle_video(self, sender=None):
         # Grab Button if not supplied
-        if sender == None:
+        if sender is None:
             self.toolbar.get_nth_item(1)
 
         # Update Button Icon & Process Change to Video
@@ -256,7 +121,7 @@ class Gui(Gtk.Grid):
 
     def toggle_audio(self, sender=None):
         # Grab Button if not supplied
-        if sender == None:
+        if sender is None:
             self.toolbar.get_nth_item(2)
 
         # Update Button Icon & Process Change to Audio
@@ -271,7 +136,7 @@ class Gui(Gtk.Grid):
 
     def toolbar_toggle_preview_visibility(self, sender=None):
         # Grab Button if not supplied
-        if sender == None:
+        if sender is None:
             self.toolbar.get_nth_item(3)
 
         # Update Button Icon
@@ -317,7 +182,6 @@ class Gui(Gtk.Grid):
         #     self.movie_window_preview.hide()
         #     self.movie_window_preview.show()
 
-
     """ Video Methods """
 
     def render_preview(self, sender):
@@ -325,21 +189,9 @@ class Gui(Gtk.Grid):
         # self.movie_window_preview.get_allocation().width,
         # self.movie_window_preview.get_allocation().height
 
-
     def render_incoming(self, sender):
         logger.debug("Temporarily Stairs")
         # self.gstreamer_stack.set_incoming_window(self.movie_window_incoming.get_window().get_xid())
-
-    def set_preview_size(self):
-        # Resize Preview
-        if self.movie_window_preview.get_size_request()[0] == -1:
-            self.movie_window_preview.set_size_request(
-                    self.movie_window_preview.get_parent().get_parent().get_allocation().width,
-                    self.movie_window_preview.get_parent().get_parent().get_allocation().height)
-        else:
-            self.movie_window_preview.set_size_request(
-                    self.movie_window_preview.get_parent().get_parent().get_allocation().width * DEFAULT_PREVIEW_SIZE,
-                    self.movie_window_preview.get_parent().get_parent().get_allocation().height * DEFAULT_PREVIEW_SIZE)
 
     def set_incoming_size(self):
         logger.debug("Temporarily Stairs")
@@ -355,38 +207,6 @@ class Gui(Gtk.Grid):
 
         # Execute Resize Logic
         self.set_preview_size()
-
-    def resized(self):
-        logger.debug("Resizing Video to match Display")
-
-        # Execute resize methods
-        self.set_incoming_size()
-        self.set_preview_size()
-
-
-    """ Chat Methods """
-
-    def get_history(self):
-        return self.chat_text.get_text(
-                self.chat_text.get_start_iter(),
-                self.chat_text.get_end_iter(),
-                True)
-
-    def chat_write_line(self, line):
-        self.chat_text.insert(self.chat_text.get_end_iter(), line, -1)
-
-    def receive_message(self, username, message):
-        self.chat_text.insert(self.chat_text.get_end_iter(), "%s [%s]: %s\n" % (username, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), message), -1)
-        self.text_view.scroll_to_iter(self.chat_text.get_end_iter(), 0.1, False, 0.0, 0.0)
-
-    def send_message(self, sender):
-        if self.chat_entry.get_text() != "":
-            message = self.chat_entry.get_text()
-            self.receive_message(self.network_stack.username, message)
-            self.network_stack.send_message(message)
-            self.chat_entry.set_text("")
-            self.chat_entry.grab_focus()
-
 
     """ Event Connected Methods """
 
@@ -419,22 +239,3 @@ class Gui(Gtk.Grid):
 
         # Add Buddy Joined Listener
         self.buddy_handler = shared_activity.connect('buddy-joined', self.buddy_left)
-
-    def connect_incoming_movie(self):
-        # Handle Incoming Construction
-        return False
-
-    def disconnect_incoming_movie(self):
-        # Handle Incoming Deconstruction
-        return False
-
-
-    """ Hacky Solutions Below This Point """
-
-    def force_redraw(self, sender):
-        # With fixed for overlay order of show() matters
-        # This method may not be necessary with GSTStreamer 1.0 and was formerly marked as "FIXME:"
-        self.toggle_incoming_visibility()
-        self.toggle_incoming_visibility()
-        self.toggle_preview_visibility()
-        self.toggle_preview_visibility()
