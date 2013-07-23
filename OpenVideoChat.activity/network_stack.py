f5801c39b78c12c07b3e954514ac17ad8a24bae8
Casey DeLorme <CDeLorme@gmail.com>
2013-07-23 16:14:07 -0400

    I know how to type (nope...), fixed name user_list to plural (users_list).

--- OpenVideoChat.activity/network_stack.py
+++ OpenVideoChat.activity/network_stack.py
@@ -181,7 +181,7 @@ class NetworkStack(object):
         logger.debug("Getting user list...")
 
         # Take the users list and send them one-by-one to the GUI?
-        pprint(user_list)
+        pprint(users_list)
 
     # def setup(self, activity, get_buddy):
     #     # Grab Shared Activity Reference
