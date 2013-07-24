43cac32ec15e485cb705706b8cdc940eeafe2b81
Casey DeLorme <CDeLorme@gmail.com>
2013-07-24 11:06:04 -0400

    Attempting another alternative expand process, as the constructor arguments fail to be accepted.

--- OpenVideoChat.activity/gui.py
+++ OpenVideoChat.activity/gui.py
@@ -114,10 +114,14 @@ class Gui(Gtk.Grid):
         self.user_list_store = Gtk.ListStore()
 
         # Create User List (Tree View)
-        user_list_tree_view = Gtk.TreeView(expand=True)
+        user_list_tree_view = Gtk.TreeView()
         user_list_tree_view.set_model(self.user_list_store)
         # user_list_tree_view.set_search_column()
         user_list_tree_view.set_search_entry(user_list_search_entry)
+
+        # Tell the Tree View to expand (won't accept these in constructor)
+        user_list_tree_view.set_vexpand(True)
+        user_list_tree_view.set_hexpand(True)
 
         # Define Storage Container & Attach Components
         user_list_grid = Gtk.Grid()
